5fabc44cbe34e83fa54e4af6c27f2753c1368bd2
armanabbassii <arman.abbasi.nt@gmail.com>
2025-12-12 22:03:36 +0330

    add first command

---
+++ src/main/java/ir/maktabsharif/springbootonlineexamsystem/SpringBootOnlineExamSystemApplication.java
@@ -0,0 +1,13 @@
+package ir.maktabsharif.springbootonlineexamsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOnlineExamSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOnlineExamSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/maktabsharif/springbootonlineexamsystem/SpringBootOnlineExamSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.maktabsharif.springbootonlineexamsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOnlineExamSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
